225095e8d164c39848ecf0c6d7e1b506bf3b0c83
Marat555 <130896231+Marat555@users.noreply.github.com>
2023-11-14 19:25:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleLabInfo() {
+    var labInfo = document.getElementById('labInfo');
+    if (labInfo.style.display === 'none') {
+        labInfo.style.display = 'block';
+    } else {
+        labInfo.style.display = 'none';
+    }
+}
